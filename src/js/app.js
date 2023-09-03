609a361aa1825b4ec5fbd35736931acdeb8b2fbe
Mathéo DELAUNAY <dseonay@gmail.com>
2023-09-03 15:55:32 +0200

    feat: "add a button to go to the top of the page"

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,21 @@
+// Select the section by its ID
+const mySection = document.getElementById('home');
+
+// Function to get and display the section's dimensions
+function getAndDisplayDimensions() {
+    const dimensions = mySection.getBoundingClientRect();
+    const height = dimensions.height;
+
+    console.log('Section height: ' + height + ' pixels');
+    return height; // Return the height for later use
+}
+
+// Call the initial function to display the dimensions when the page loads
+const sectionHeight = getAndDisplayDimensions();
+
+// Add an event listener to monitor window resize events
+window.addEventListener('resize', getAndDisplayDimensions);
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
@@ -12,8 +30,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-const themeBtn = document.querySelector('.theme-btn'); //creation de themeBtn et le liee a la class theme-btn
-themeBtn.addEventListener('click',() =>{ //ecouter l'event click sur le btn
-    let element = document.body;  // declacrqation de la variable element sur le bloc courant
-    element.classList.toggle('light-mode') //active le mode "light-mode" en toggle donc activation <---> desactivation
-})
+// Select the anchor link button by its ID
+const anchorLinkButton = document.getElementById('anchor-link-button');
+
+// Function to show or hide the button based on scrolling
+function handleScroll() {
+    const currentScrollPosition = window.scrollY;
+
+    // Use the sectionHeight variable for comparison
+    if (currentScrollPosition >= sectionHeight) {
+        anchorLinkButton.classList.add('active');
+    } else {
+        anchorLinkButton.classList.remove('active');
+    }
+}
+
+// Add an event listener to handle page scrolling
+window.addEventListener('scroll', handleScroll);
